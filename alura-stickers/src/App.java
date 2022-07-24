1c04434dc646b80f1532cb9d1c7a158a12b87511
flaviobp <flaviobp@gmail.com>
2022-07-24 11:26:18 -0300

    teste123

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -13,7 +13,7 @@ public class App {
         // fazer uma conexão HTTP e buscar os top 250 filmes
         // String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
-        URI endereco = URI.create(url);
+        URI endereco = URI.create(url); 
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
